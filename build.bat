8a123438d369deb460dab4eec207644a7c5d588a
ARMmaster17 <wordman05@gmail.com>
2015-05-13 09:59:49 -0400

    Fixed build script

--- build.bat
+++ build.bat
@@ -1,27 +1,33 @@
+echo Setting up dependencies
+set PATH=C:\Users\Joshua\Documents\GitHub\HiveOS\tools\DJGPP\bin;%PATH%
+set PATH=C:\Users\Joshua\Documents\GitHub\HiveOS\tools\NASM;%PATH%
+set DJGPP=C:\Users\Joshua\Documents\GitHub\HiveOS\tools\DJGPP\djgpp.env
 echo Now assembling, compiling, and linking your kernel:
 nasm -f aout -o start.o start.asm
-rem Remember this spot here: We will add 'gcc' commands here to compile C sources
+echo Building OS
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o main.o main.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o main.o main.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o scrn.o scrn.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o scrn.o scrn.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o gdt.o gdt.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o gdt.o gdt.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o idt.o idt.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o idt.o idt.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o isrs.o isrs.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o isrs.o isrs.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o irq.o irq.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o irq.o irq.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o timer.o timer.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o timer.o timer.c
 
-gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I./include -c -o kb.o kb.c
+gcc -Wall -O -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin -I "\include" -c -o kb.o kb.c
 
-rem This links all your files. Remember that as you add *.o files, you need to
-rem add them after start.o. If you don't add them at all, they won't be in your kernel!
+echo Linking files
 ld -T link.ld -o kernel.bin start.o main.o scrn.o gdt.o idt.o isrs.o irq.o timer.o kb.o
+echo Moving finished files
+xcopy kernel.bin bin\kernel.bin /E /I
 echo Cleaning up object files...
 del *.o
+del kernel.bin
 echo Done!
 pause
